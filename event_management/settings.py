be823cafb6fc2bcec3b298a8ffc5cc509ee4f8e8
Tashib04 <tashibunique@gmail.com>
2026-03-06 14:23:03 +0600

    debug fixes

--- event_management/settings.py
+++ event_management/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-event-management-secret-key-change-in-production'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
